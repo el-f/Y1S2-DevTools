3d39934a947febba0faa6b106c7c77da951fb495
Elfein7Night <ImmortalisEL@gmail.com>
2020-04-07 18:01:40 +0300

    added test unit

---
+++ src/tests/firstTest.java
@@ -0,0 +1,17 @@
+package tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class firstTest {
+
+    @Test
+    public void testSomething() {
+
+        Assert.assertEquals(true, true);
+        Assert.assertEquals(3, 1 + 2);
+
+
+    }
+
+}
