a2c044e857ea108564a171095f50d04243d8c628
Elfein7Night <ImmortalisEL@gmail.com>
2020-04-07 16:16:05 +0300

    To do list

--- src/AirportProject/Flight.java
+++ src/AirportProject/Flight.java
@@ -4,41 +4,48 @@ import java.time.LocalDate;
 
 public class Flight {
 
-	private String flightNum;
-	private Plane plane;
-	private LocalDate date;
-	private String origin;
-	private String destination;
+    private String flightNum;
+    private Plane plane;
+    private LocalDate date;
+    private String origin;
+    private String destination;
 
-	public Flight(String _flightNum, Plane _plane, LocalDate _date, String _origin, String _destination) {
-		flightNum = _flightNum;
-		plane = _plane;
-		date = _date;
-		origin = _origin;
-		destination = _destination;
-	}
+    public Flight(String _flightNum, Plane _plane, LocalDate _date, String _origin, String _destination) {
+        flightNum = _flightNum;
+        plane = _plane;
+        date = _date;
+        setDirection(_origin, _destination);
 
-	public Flight() {
-		this("", null, null, "", "");
-	}
+    }
 
-	public LocalDate getDate() {
-		return date;
-	}
+    private void setDirection(String _origin, String _destination) {
+        if (_destination.toLowerCase().equals("israel") || _origin.toLowerCase().equals("israel")) {
+            destination = _destination;
+            origin = _origin;
+        }
+    }
 
-	public int getNumPassengers() {
-		return plane.getNumPassengers();
-	}
+    public Flight() {
+        this("", null, null, "", "");
+    }
 
-	public void changeDirection() {
-		String temp = origin;
-		origin = destination;
-		destination = temp;
-	}
+    public LocalDate getDate() {
+        return date;
+    }
 
-	public String toString() {
-		return "Flight: " + flightNum + " | " + plane.toString() + ", date: " + date + ", Origin: " + origin
-				+ ", Destination: " + destination;
-	}
+    public int getNumPassengers() {
+        return plane.getNumPassengers();
+    }
+
+    public void changeDirection() {
+        String temp = origin;
+        origin = destination;
+        destination = temp;
+    }
+
+    public String toString() {
+        return "Flight: " + flightNum + " | " + plane.toString() + ", date: " + date + ", Origin: " + origin
+                + ", Destination: " + destination;
+    }
 
 }
