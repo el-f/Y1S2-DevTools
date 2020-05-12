dd91a47abd5439e4de691a8e4f2ba07f7c8d2acf
Elfein7Night <ImmortalisEL@gmail.com>
2020-05-12 21:10:55 +0300

    code cleanup

--- src/AirportProject/Menu.java
+++ src/AirportProject/Menu.java
@@ -10,82 +10,80 @@ public class Menu {
     public static void showMenu() {
         ap = new Airport();
         Scanner s = new Scanner(System.in);
-        int choice;
-        try {
-
-
-            do {
-                System.out.println("Menu:");
-                System.out.println("1) Create outgoing Flight");
-                System.out.println("2) Create incoming Flight");
-                System.out.println("3) Show all outgoing flights");
-                System.out.println("4) Show all incoming flights");
-                System.out.println("5) Save airport to file");
-                System.out.println("6) Load airport from file");
-                System.out.println("7) Show Custom Selected flights");
-                System.out.println("8) Show all flights");
-                System.out.println("\n0) To exit");
-                choice = s.nextInt();
-                switch (choice) {
-                    case 1:
-                        ap.getFlightFromUser(s, true);
-                        break;
-                    case 2:
-                        ap.getFlightFromUser(s, false);
-                        break;
-                    case 3:
-                        System.out.println("Outgoing flights: ");
-                        ap.showOutgoingFlights();
-                        break;
-                    case 4:
-                        System.out.println("Incoming flights: ");
-                        ap.showIncomingFlights();
-                        break;
-                    case 5:
-                        System.out.println("Please enter file name or file path to create");
-                        ap.save(s.next());
-                        break;
-                    case 6:
-                        System.out.println("Choose: ");
-                        System.out.println("1) enter file name/path yourself");
-                        System.out.println("2) load default file");
-                        switch (s.nextInt()) {
-                            case 1:
-                                System.out.println("Please enter file name or file path to read from");
-                                File f = new File(s.next());
-                                if (!f.exists()) {
-                                    System.out.println("no such file!");
-                                    break;
-                                }
-                                ap = new Airport(f);
+        int choice = -1;
+        while (choice != 0) try {
+            System.out.println("Menu:");
+            System.out.println("1) Create outgoing Flight");
+            System.out.println("2) Create incoming Flight");
+            System.out.println("3) Show all outgoing flights");
+            System.out.println("4) Show all incoming flights");
+            System.out.println("5) Save airport to file");
+            System.out.println("6) Load airport from file");
+            System.out.println("7) Show Custom Selected flights");
+            System.out.println("8) Show all flights");
+            System.out.println("\n0) To exit");
+            choice = s.nextInt();
+            switch (choice) {
+                case 1:
+                    ap.getFlightFromUser(s, true);
+                    break;
+                case 2:
+                    ap.getFlightFromUser(s, false);
+                    break;
+                case 3:
+                    System.out.println("Outgoing flights: ");
+                    ap.showOutgoingFlights();
+                    break;
+                case 4:
+                    System.out.println("Incoming flights: ");
+                    ap.showIncomingFlights();
+                    break;
+                case 5:
+                    System.out.println("Please enter file name or file path to create");
+                    ap.save(s.next());
+                    break;
+                case 6:
+                    System.out.println("Choose: ");
+                    System.out.println("1) enter file name/path yourself");
+                    System.out.println("2) load default file");
+                    switch (s.nextInt()) {
+                        case 1:
+                            System.out.println("Please enter file name or file path to read from");
+                            File f = new File(s.next());
+                            if (!f.exists()) {
+                                System.out.println("no such file!");
                                 break;
-                            case 2:
-                                ap = new Airport(new File("airport"));
-                                System.out.println("Loaded default file!");
-                                break;
-                            default:
-                                System.out.println("Invalid Input");
-                                break;
-                        }
-                        break;
-                    case 7:
-                        showCustomRangeFlights(s);
-                        break;
-                    case 8:
-                        ap.show();
-                        break;
-                    case 0:
-                        System.out.println("~~~end of program~~~");
-                        break;
-                    default:
-                        System.out.println("invalid input");
-                        break;
-                }
-            } while (choice != 0);
-        } catch (MyException e) {
+                            }
+                            ap = new Airport(f);
+                            break;
+                        case 2:
+                            ap = new Airport(new File("airport"));
+                            System.out.println("Loaded default file!");
+                            break;
+                        default:
+                            System.out.println("Invalid Input");
+                            break;
+                    }
+                    break;
+                case 7:
+                    showCustomRangeFlights(s);
+                    break;
+                case 8:
+                    ap.show();
+                    break;
+                case 0:
+                    System.out.println("~~~end of program~~~");
+                    break;
+                default:
+                    System.out.println("invalid input");
+                    break;
+            }
+        } catch (
+                MyException e) {
             System.out.println(e.getMessage());
             System.out.println("please try again");
-        } catch (Exception e) {
+        } catch (
+                Exception e) {
             System.out.println("oops! error: " + e.getClass().getSimpleName());
             System.out.println("please try again");
         }
