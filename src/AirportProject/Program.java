af22a6024e5571bc3ad63ac86feba53b17a422ca
Elfein7Night <ImmortalisEL@gmail.com>
2020-05-31 23:33:54 +0300

    all filters done in web

--- src/AirportProject/Program.java
+++ src/AirportProject/Program.java
@@ -1,25 +1,29 @@
 package AirportProject;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Program {
+
+
     public static void main(String[] args) {
         boolean direction = args.length > 0 && (args[0].equalsIgnoreCase("departures") ||
                 args[0].equalsIgnoreCase("arrivals"));
-//        boolean country = args.length > 2;
-//        boolean city = args.length > 3;
-//        boolean airport = args.length > 4;
         if (args.length == 0)
             Menu.showMenu();
         else {
+//            System.out.println("args:" + "<br>");
+//            for (String arg : args)
+//                System.out.println(arg + "<br>");
+            DateTimeFormatter ldtFormatter = DateTimeFormatter.ofPattern("yyyy/MM/dd-HH:mm:ss");
             LocalDateTime D1 = LocalDateTime.of(2020, 11, 20, 2, 3);
             LocalDateTime D2 = LocalDateTime.of(2020, 11, 21, 14, 30);
             LocalDateTime D3 = LocalDateTime.of(2020, 11, 22, 19, 59);
             LocalDateTime D4 = LocalDateTime.of(2020, 11, 23, 11, 16);
-            // plane
+            // planes
             Plane P1 = new Plane("Wright-Flyer", 4);
             Plane P2 = new Plane("Supermarine-Spitfire", 15);
             Plane P3 = new Plane("Boeing-787", 150);
@@ -28,7 +32,7 @@ public class Program {
             Plane P6 = new Plane("Boeing_B-29", 120);
             Plane P7 = new Plane("F-35", 2);
 
-            // flight
+            // flights
             Flight F1 = new Flight(3, "SAD1978", P1, D1, "Albania", "albania_city", "albania_airport", true);
             Flight F2 = new Flight(1, "HASF1238", P2, D1, "Austria", "Austria_city", "Austria_airport", false);
             Flight F3 = new Flight(2, "ASD41IDA", P3, D2, "Guinea", "Guinea_city", "Guinea_airpot", true);
@@ -36,14 +40,11 @@ public class Program {
             Flight F5 = new Flight(2, "DFWQ41F", P5, D4, "Italy", "Italy_city", "Italy_airport", true);
             Flight F6 = new Flight(3, "UR24EE", P6, D3, "Laos", "Laos_city", "Laos_airport", false);
             Flight F7 = new Flight(1, "KSD556Q", P7, D1, "Iran", "Iran_city", "Iran_airport", true);
+            Flight F8 = new Flight(1, "KS9782E", P3, D4, "UK", "London", "london_airport", true);
 
             Airport ap = new Airport();
-            ap.getFlights().addAll(Arrays.asList(F1, F2, F3, F4, F5, F6, F7));
+            ap.getFlights().addAll(Arrays.asList(F1, F2, F3, F4, F5, F6, F7, F8));
             List<Flight> result = new ArrayList<>(ap.getFlights());
-            var oc = new Object() {
-                String str;
-                int num;
-            };
             if (direction) {
                 switch (args[0].toLowerCase()) {
                     case "departures":
@@ -54,53 +55,37 @@ public class Program {
                         break;
                 }
             }
-//            if (args[]) {
-//                oc.str = s.next().toLowerCase();
-//                result.removeIf(f -> !f.getCountry().toLowerCase().equals(oc.str));
-//            }
-//            System.out.println("Do you want to filter by city?");
-//            if (scanBoolean(s)) {
-//                System.out.println("Please Enter city:");
-//                oc.str = s.next().toLowerCase();
-//                result.removeIf(f -> !f.getCity().toLowerCase().equals(oc.str));
-//            }
-//            System.out.println("Do you want to filter by airport?");
-//            if (scanBoolean(s)) {
-//                System.out.println("Please Enter airport name:");
-//                oc.str = s.next().toLowerCase();
-//                result.removeIf(f -> !f.getAirportName().toLowerCase().equals(oc.str));
-//            }
-//            System.out.println("Do you want to filter by terminal num?");
-//            if (scanBoolean(s)) {
-//                System.out.println("Please Enter terminal number:");
-//                oc.num = s.nextInt();
-//                result.removeIf(f -> f.getTerminal() != oc.num);
-//            }
-//            System.out.println("Do you want to filter by day of the week?");
-//            if (scanBoolean(s)) {
-//                System.out.println("Please enter day of the week by name");
-//                oc.str = s.next();
-//                result.removeIf(f -> !f.getDate().getDayOfWeek().name().equals(oc.str.toUpperCase()));
-//            }
-//            System.out.println("Do you want to filter by date and time range?");
-//            if (scanBoolean(s)) {
-//                System.out.println("Please enter start date");
-//                LocalDateTime start = Flight.getDateTimeFromUser(s);
-//                System.out.println("Please enter end date");
-//                LocalDateTime end = Flight.getDateTimeFromUser(s);
-//                result.removeIf(f -> f.getDate().isBefore(start) || f.getDate().isAfter(end));
-//            }
-            result.forEach(r->System.out.println(r +"<br>"));
+            if (!args[1].isBlank()) {
+                result.removeIf(f -> !f.getCountry().equalsIgnoreCase(args[1]));
+            }
+            if (!args[2].isBlank()) {
+                result.removeIf(f -> !f.getCity().toLowerCase().equals(args[2]));
+            }
+            if (!args[3].isBlank()) {
+                result.removeIf(f -> !f.getAirportName().toLowerCase().equals(args[3]));
+            }
+            if (!args[4].isBlank()) {
+                result.removeIf(f -> f.getTerminal() != Integer.parseInt(args[4]));
+            }
+            if (!args[5].isBlank()) {
+                result.removeIf(f -> !f.getDate().getDayOfWeek().name().equalsIgnoreCase(args[5]));
+            }
+            if (!args[6].isBlank()) {
+                result.removeIf(f -> f.getDate().isBefore(LocalDateTime.parse(args[6], ldtFormatter)));
+            }
+            if (!args[7].isBlank()) {
+                result.removeIf(f -> f.getDate().isAfter(LocalDateTime.parse(args[7], ldtFormatter)));
+            }
+            if (result.isEmpty())
+                System.out.println("empty flights list! too much or invalid filters!");
+            result.forEach(r -> System.out.println(r + "<br>"));
         }
-//
-//
-////        // remove by ref
-////        System.out.println("\nremoved even numbers:");
-////        ap.removeFlight(F2);
-////        ap.removeFlight(F4);
-////        ap.removeFlight(F6);
-////        ap.show();
-//
+
+
+
+/*
+          method examples:
+ */
 //        System.out.println("\nsorted by number of Passengers:");
 //        ap.sortByNumOfPassengers();
 //        ap.show();
@@ -125,7 +110,6 @@ public class Program {
 //        System.out.println("\nairport 2");
 //        ap2.show();
 
-//    }
 
     }
 }
--- webInit/WebInit.py
+++ webInit/WebInit.py
@@ -11,16 +11,17 @@ def airport():
     return subprocess.check_output(["java", "-classpath", "C:/Users/Elazar/Documents/GitHub/Y1S2-Homework/bin",
                                     "AirportProject.Program",
                                     request.args.get('d'),  # direction
-                                    # request.args.get('cnt'),  # country
-                                    # request.args.get('cty'),  # city
-                                    # request.args.get('ap'),   # airport
-                                    # request.args.get('trm'),  # terminal
-                                    # request.args.get('wd'),   # dayofweek
-                                    # request.args.get('rng')   # date range
+                                    request.args.get('cnt'),  # country
+                                    request.args.get('cty'),  # city
+                                    request.args.get('ap'),  # airport
+                                    request.args.get('trm'),  # terminal
+                                    request.args.get('wd'),  # dayofweek
+                                    request.args.get('start'),  # date range lower bound
+                                    request.args.get('end')  # date range higher bound
                                     ])
 
 
 app.run(port=8000, host="0.0.0.0")
 
-# http://localhost:8000/?d=arrivals
-# http://127.0.0.1:5000/?d=arrivals
+# http://localhost:8000/?d=departures&cnt=&cty=&ap=&trm=&wd=&start=&end=2020/11/21-22:22:22
+# http://localhost:8000/?d=&cnt=&cty=&ap=&trm=&wd=&start=&end=
