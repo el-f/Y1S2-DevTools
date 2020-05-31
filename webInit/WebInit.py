1c6a586c9c48e971ba92cffc46fe63c0d01b0778
Elfein7Night <ImmortalisEL@gmail.com>
2020-05-31 23:45:06 +0300

    better method calling standards

--- webInit/WebInit.py
+++ webInit/WebInit.py
@@ -10,14 +10,14 @@ app = Flask("__name__")
 def airport():
     return subprocess.check_output(["java", "-classpath", "C:/Users/Elazar/Documents/GitHub/Y1S2-Homework/bin",
                                     "AirportProject.Program",
-                                    request.args.get('d'),  # direction
-                                    request.args.get('cnt'),  # country
-                                    request.args.get('cty'),  # city
-                                    request.args.get('ap'),  # airport
-                                    request.args.get('trm'),  # terminal
-                                    request.args.get('wd'),  # dayofweek
+                                    request.args.get('d'),      # direction
+                                    request.args.get('cnt'),    # country
+                                    request.args.get('cty'),    # city
+                                    request.args.get('ap'),     # airport
+                                    request.args.get('trm'),    # terminal
+                                    request.args.get('wd'),     # dayofweek
                                     request.args.get('start'),  # date range lower bound
-                                    request.args.get('end')  # date range higher bound
+                                    request.args.get('end')     # date range higher bound
                                     ])
 
 
