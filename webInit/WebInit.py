f18c08ff2d5fda49187d9717bfce2440d0c9f935
Gandalf <Gandalf@DESKTOP-40ETVDN.lan>
2020-06-02 17:31:23 +0300

    added weekdays

--- webInit/WebInit.py
+++ webInit/WebInit.py
@@ -26,7 +26,13 @@ def airport():
                                     request.args.get('city'),       # city
                                     request.args.get('airport'),    # airport
                                     request.args.get('terminal'),   # terminal
-                                    request.args.get('weekday'),    # dayofweek
+                                    request.args.get('sunday'),    # dayofweek
+                                    request.args.get('monday'),    # dayofweek
+                                    request.args.get('tuesday'),    # dayofweek
+                                    request.args.get('wednesday'),    # dayofweek
+                                    request.args.get('thursday'),    # dayofweek
+                                    request.args.get('friday'),    # dayofweek
+                                    request.args.get('saturday'),    # dayofweek
                                     request.args.get('start'),      # date range lower bound
                                     request.args.get('end'),        # date range higher bound
                                     request.args.get('airline')     # company
