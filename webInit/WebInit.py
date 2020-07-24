80d84cc7fb615a306a9f7298ec275cf81d3f1321
Elfein7Night <ImmortalisEL@gmail.com>
2020-07-24 20:20:11 +0300

    Code Cleanup

--- webInit/WebInit.py
+++ webInit/WebInit.py
@@ -62,10 +62,10 @@ def all_flights():
                                     "AirportProject.Program",
                                     request.args.get('outformat'),
                                     "all",
-                                    request.args.get('country'),  # country
-                                    request.args.get('city'),  # city
-                                    request.args.get('airport'),  # airport
-                                    request.args.get('airline'),  # company
+                                    request.args.get('country'),    # country
+                                    request.args.get('city'),       # city
+                                    request.args.get('airport'),    # airport
+                                    request.args.get('airline'),    # company
                                     request.args.get('day1'), request.args.get('month1'), request.args.get('year1'),
                                     request.args.get('day2'), request.args.get('month2'), request.args.get('year2'),
                                     request.args.get('sunday'), request.args.get('monday'),
@@ -84,3 +84,8 @@ if __name__ == "__main__":
 # http://localhost:8000/arrivals?outformat=html&country=&city=&airport=&airline=&day1=&month1=&year1=&day2=&month2=&year2=&sunday=&monday=&tuesday=&wednesday=&thursday=&friday=&saturday=
 # http://localhost:8000/departures?outformat=html&country=&city=&airport=&airline=&day1=&month1=&year1=&day2=&month2=&year2=&sunday=&monday=&tuesday=&wednesday=&thursday=&friday=&saturday=
 # http://localhost:8000/departures?outformat=html&country=france&city=paris&airport=CDG&airline=elal&day1=4&month1=6&year1=2020&day2=31&month2=7&year2=2020&sunday=true&monday=false&tuesday=true&wednesday=true&thursday=false&friday=false&saturday=false
+
+# command line example:
+# curl "http://localhost:8000/all?outformat=text&country=&city=&airport=&airline=&day1=&month1=&year1=&day2=&month2=&
+#       year2=&sunday=&monday=&tuesday=&wednesday=&thursday=&friday=&saturday="
+#
