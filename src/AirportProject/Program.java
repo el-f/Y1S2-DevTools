d1bbc50a8487d7622961d68fc549d89a8e42bf47
Elfein7Night <ImmortalisEL@gmail.com>
2020-07-26 17:02:55 +0300

    Code cleanup and organization

--- src/AirportProject/Program.java
+++ src/AirportProject/Program.java
@@ -18,49 +18,47 @@ public class Program {
         if (args.length == 0)
             Menu.showMenu();
         else {
-            boolean direction = (args[1].equalsIgnoreCase("departures") ||
-                    args[1].equalsIgnoreCase("arrivals"));
-
-            //external java call has to have full path.
+            //external java calls need full path, internal tests only need filename.
             String path;
-            if (args[0].equalsIgnoreCase(firstTest.identifier)) //internal tests only need filename
-                path = "airport";
+            if (args[0].equalsIgnoreCase(firstTest.identifier)) path = defaultFile;
             else path = Paths.get("").toAbsolutePath().getParent() + "\\" + defaultFile;
             Airport ap = new Airport(new File(path));
-            List<Flight> result = new ArrayList<>(ap.getFlights());
 
-            if (direction) {
-                switch (args[1].toLowerCase()) {
-                    case "departures":
-                        result.removeAll(ap.getIncomingFlights());
-                        break;
-                    case "arrivals":
-                        result.removeAll(ap.getOutgoingFlights());
-                        break;
-                    default:
-                        break;
-                }
-            }
-            if (!args[2].isBlank()) {
-                Airport.filterByCountry(result, args[2]);
-            }
-            if (!args[3].isBlank()) {
-                Airport.filterByCity(result, args[3]);
-            }
-            if (!args[4].isBlank()) {
-                Airport.filterByAirport(result, args[4]);
-            }
-            if (!args[5].isBlank()) {
-                Airport.filterByCompany(result, args[5]);
-            }
-            if (!args[6].isBlank() && !args[7].isBlank() && !args[8].isBlank()) {
-                Airport.filterByStartDate(result, Flight.getDateTimeFromUser(parseInt(args[6]),
-                        parseInt(args[7]), parseInt(args[8])));
-            }
-            if (!args[9].isBlank() && !args[10].isBlank() && !args[11].isBlank()) {
-                Airport.filterByEndDate(result, Flight.getDateTimeFromUser(parseInt(args[9]),
-                        parseInt(args[10]), parseInt(args[11])));
-            }
+            List<Flight> results = new ArrayList<>(ap.getFlights());
+
+            if (args[1].equalsIgnoreCase("departures"))
+                results.removeAll(ap.getIncomingFlights());
+            else if (args[1].equalsIgnoreCase("arrivals"))
+                results.removeAll(ap.getOutgoingFlights());
+
+            if (!args[2].isBlank()) Airport.filterByCountry(results, args[2]);
+
+            if (!args[3].isBlank()) Airport.filterByCity(results, args[3]);
+
+            if (!args[4].isBlank()) Airport.filterByAirport(results, args[4]);
+
+            if (!args[5].isBlank()) Airport.filterByCompany(results, args[5]);
+
+            if (!args[6].isBlank() && !args[7].isBlank() && !args[8].isBlank())
+                Airport.filterByStartDate(
+                        results,
+                        Flight.getDateTimeFromUser(
+                                parseInt(args[6]),
+                                parseInt(args[7]),
+                                parseInt(args[8])
+                        )
+                );
+
+            if (!args[9].isBlank() && !args[10].isBlank() && !args[11].isBlank())
+                Airport.filterByEndDate(
+                        results,
+                        Flight.getDateTimeFromUser(
+                                parseInt(args[9]),
+                                parseInt(args[10]),
+                                parseInt(args[11])
+                        )
+                );
+
             String weekdays = "";
             if (!args[12].isBlank() && Boolean.parseBoolean(args[12])) weekdays += "sunday ";
             if (!args[13].isBlank() && Boolean.parseBoolean(args[13])) weekdays += "monday ";
@@ -69,14 +67,15 @@ public class Program {
             if (!args[16].isBlank() && Boolean.parseBoolean(args[16])) weekdays += "thursday ";
             if (!args[17].isBlank() && Boolean.parseBoolean(args[17])) weekdays += "friday ";
             if (!args[18].isBlank() && Boolean.parseBoolean(args[18])) weekdays += "saturday ";
-            if (weekdays.length() > 0)
-                Airport.filterByWeekDays(result, weekdays);
 
-            if (result.isEmpty())
+            if (weekdays.length() > 0) Airport.filterByWeekDays(results, weekdays);
+
+            if (results.isEmpty())
                 System.out.println("empty flights list! too much or invalid filters!");
-            result.forEach(r -> System.out.println(r + (
+            else results.forEach(result -> System.out.println(result + (
                     args[0].equalsIgnoreCase("TEXT") ? "" :
-                            args[0].equalsIgnoreCase("HTML") ? "<br>" : "")));
+                            args[0].equalsIgnoreCase("HTML") ? "<br>" : ""))
+            );
         }
 
 //        initDefault();        //for initiating the default files.
