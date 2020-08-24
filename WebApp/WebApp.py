7f750dda4ecf8b8a0c88f74378446ef85c3e99c9
Elfein7Night <ImmortalisEL@gmail.com>
2020-08-24 22:55:46 +0300

    Days checkboxes now on by default for better UX. + General Code Cleanup

--- WebApp/WebApp.py
+++ WebApp/WebApp.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import os
+import secrets
 import subprocess
 
 from flask import Flask, request, render_template, flash, url_for
@@ -7,9 +8,14 @@ from forms import FlightsForm
 
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = '1074ef660db4d179d79feecf0ace6a51'
+app.config['SECRET_KEY'] = secrets.token_hex(16)
 
 path = os.getcwd() + "/../target/classes"  # get parent of working dir then go to classes folder
+
+command = [
+    "java", "-classpath", path,
+    "AirportProject.Program",  # Package.Class_File
+]
 
 
 def bool2str(boolean: bool):
@@ -33,15 +39,14 @@ def get_response_for_args(
 ):
     return display_results(
         flights=subprocess.check_output(
-            ["java", "-classpath", path,
-             "AirportProject.Program",  # Package.Class_File
-             "HTML",
-             direction, country, city, airport, airline,
-             day1, month1, year1,
-             day2, month2, year2,
-             sunday, monday, tuesday, wednesday, thursday, friday, saturday
-             # terminal
-             ]).decode().split("<br>")  # decode() - decode bytes to string
+            command + [
+                "HTML",
+                direction, country, city, airport, airline,
+                day1, month1, year1,
+                day2, month2, year2,
+                sunday, monday, tuesday, wednesday, thursday, friday, saturday
+                # terminal
+            ]).decode().split("<br>")  # decode() - decode bytes to string
 
     )
 
@@ -86,13 +91,13 @@ def get_url(direction):
            + direction + "</a><br>"
 
 
-def go_back(step=""):
-    return "<a href=http://localhost:8000/" + step + ">Go Back</a><br><br>"
+def go_back_url(step="", outformat=""):
+    return "" if outformat == "text" else "<a href=http://localhost:8000/" + step + ">Go Back</a><br><br>"
 
 
 @app.route("/legacy")
 def legacy():
-    return go_back() + "Example links:<br>" + get_url("Arrivals") + get_url("Departures") + get_url("All")
+    return go_back_url() + "Example links:<br>" + get_url("Arrivals") + get_url("Departures") + get_url("All")
 
 
 @app.route("/departures")
@@ -107,13 +112,14 @@ def arrivals():
 
 @app.route("/all")
 def all_flights(direction="all"):
-    return go_back("legacy").encode() + \
-           subprocess.check_output(["java", "-classpath", path,
-                                    "AirportProject.Program",  # Package.Class_File
-                                    request.args.get('outformat'),
-                                    direction
-                                    ] + list(request.args.values())[1:]
-                                   )
+    outformat = request.args.get('outformat')
+    return go_back_url("legacy", outformat).encode() + subprocess.check_output(
+        command
+        + [
+            outformat,
+            direction
+        ] + list(request.args.values())[1:]  # [1:] - remove first element
+    )
 
 
 if __name__ == "__main__":
